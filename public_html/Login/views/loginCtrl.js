dafd048b64b381bf4fecb0e02bf8a9902c5374e0
btholmes <btholmes@iastate.edu>
2017-02-24 20:40:10 -0600

    Set up

---
+++ public_html/Login/views/loginCtrl.js
@@ -0,0 +1,18 @@
+(function(){
+    'use strict';
+
+    angular.module("myApp" )
+        .controller("loginCtrl",loginCtrl);
+
+    loginCtrl.$inject = ['$scope'];
+
+    function loginCtrl($scope){
+        var vm = this;
+
+
+
+
+
+    }
+
+})();
---
+++ public_html/Places/controllers/placesCtrl.js
@@ -0,0 +1,18 @@
+(function(){
+    'use strict';
+
+    angular.module("myApp" )
+        .controller("placesCtrl",placesCtrl);
+
+    placesCtrl.$inject = ['$scope'];
+
+    function placesCtrl($scope){
+        var vm = this;
+
+
+
+
+
+    }
+
+})();
